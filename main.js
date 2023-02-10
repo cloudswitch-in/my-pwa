472288ccd66b3c35fbe094abd30eeda7e5121b3c
CS Admin <admin@cloudswitch.in>
2023-02-10 23:33:22 +0530

    install code

--- main.js
+++ main.js
@@ -9,3 +9,24 @@ if ('serviceWorker' in navigator) {
     });
   }
   
+
+  // Check if the browser supports PWA installation
+if (window.navigator.standalone === false) {
+  // Show a prompt to the user to install your PWA
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    const installPrompt = e;
+    const installButton = document.querySelector('.js-install-button');
+    installButton.addEventListener('click', () => {
+      installPrompt.prompt();
+      installPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the installation prompt');
+        } else {
+          console.log('User dismissed the installation prompt');
+        }
+      });
+    });
+    installButton.style.display = 'block';
+  });
+}
