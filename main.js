1359c581c5e1dad8f3df02f626160274496abe97
CS Admin <admin@cloudswitch.in>
2023-02-10 23:47:51 +0530

    sw path Fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Register the Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
         console.log('Service Worker registered: ', registration);
       }, function(err) {
         console.log('Service Worker registration failed: ', err);
